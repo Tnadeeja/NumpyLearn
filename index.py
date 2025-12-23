93bcb60ecc3fe17bd2487258b790a8667990b011
nadeeja <nadeejatw@gmail.com>
2025-12-23 22:58:00 +0530

    index

---
+++ index.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5,6])
+print(arr1)
+print(arr1[0])
+print(arr1[-1])
+
+arr2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr2)
+print(arr2[1,0])
+
+arr3 = np.array([[[1,2,3],[5,6,7]],[[4,5,6],[4,6,9]], [[7,8,9],[1,7,6]]])
+print(arr3)
+print(arr3[2,0,1])
+print(arr3[1,1,2])
